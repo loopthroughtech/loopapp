8bfc0fb14a581501092e41f308c9c52e0fd9e036
P K Aravindkrishnan <65278075+Aravind-pk@users.noreply.github.com>
2021-02-03 10:02:43 +0530

    spline

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,6 +31,7 @@ export default function IndexPage() {
           {/* <SecurePayment />
           <Package /> */}
           <Faq />
+          <iframe src='spline/index.html' frameBorder='0' width='100%' height='600px'></iframe>
         </Layout>
       </StickyProvider>
     </ThemeProvider>
